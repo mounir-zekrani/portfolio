0d9670d19975e546d1d1f086349a8c0bb2c15d1e
mounir mounir <zekranimounir73@gmail.com>
2024-03-15 16:40:25 +0000

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas} from './components';
 
 const App = () => {
